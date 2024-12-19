5168d90421745b6e12c4cb07ad8147ed2628ac05
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-19 19:36:36 +0530

    Update main.tf

--- Remote_State/main.tf
+++ Remote_State/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # S3 Bucket for Terraform state
 resource "aws_s3_bucket" "tf_state" {
-  bucket        = "unique-terraform-state-bucket-hcl-12345"
+  bucket        = "unique-terraform-state-bucket-hcl-54673"
   acl           = "private"
 
   versioning {
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "tf_state_lock" {
-  name         = "unique-terraform-state-lock-hcl-12345"
+  name         = "unique-terraform-state-lock-hcl-54673"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
