18255c6c14a3444ff86fa2c947a11eaa4f217bce
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-19 13:24:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 # main.tf
 
+# Provider for the S3 bucket (in ap-southeast-2 region)
 provider "aws" {
-  region = "us-east-1"  # Use your desired AWS region
+  alias  = "s3"
+  region = "ap-southeast-2"
+}
+
+# Provider for DynamoDB (in us-east-1 region)
+provider "aws" {
+  alias  = "dynamodb"
+  region = "us-east-1"
 }
 
 # Remote Backend Configuration
@@ -9,10 +17,9 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-hcl"   # Your S3 bucket name
     key            = "terraform/statefile/terraform.tfstate"   # Path to store the state file in S3
-    region         = "ap-southeast-2"                    # AWS region for the state storage
-    encrypt        = true                           # Enable encryption for state file
-    dynamodb_table = "terraform-state-locks"        # DynamoDB table for state locking
-    versioning     = true                           # Enable versioning to track state changes
+    region         = "ap-southeast-2"                    # Region for the S3 bucket
+    encrypt        = true                                # Enable encryption for state file
+    versioning     = true                                # Enable versioning for state tracking
+    dynamodb_table = "terraform-state-locks"             # DynamoDB table for state locking
   }
 }
-i
