9734aa7c592782f2bc30c64e83f428f8defcd504
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-19 19:42:27 +0530

    Update backend.tf

--- Remote_State/backend.tf
+++ Remote_State/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "unique-terraform-state-bucket-hcl-3210"
+    bucket         = "my-test-bucket-hcl"
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "unique-terraform-state-lock-hcl-3210"
-    encrypt        = true
   }
 }
