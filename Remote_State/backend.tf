65c07895f7763b3f38b7096ad7adf0e283c4659c
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-19 19:12:24 +0530

    Update backend.tf

--- Remote_State/backend.tf
+++ Remote_State/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-hcl"
+    bucket         = "unique-terraform-state-bucket-hcl-12345"
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
   }
